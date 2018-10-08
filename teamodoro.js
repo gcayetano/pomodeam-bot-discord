d7a7e5096783951e07cab3e29282d7e426ef93f2
Gabriel Cayetano <gcayetanonovalio@gmail.com>
2018-10-08 20:32:58 +0200

    add pomodoro methods and logic

---
+++ teamodoro.js
@@ -0,0 +1,52 @@
+class Teamodoro {
+  constructor(bot, channel) {
+    this.lastState = null;
+    this.lastMinute = null;
+    this.timeDifference = 0; 
+
+    this.bot = bot;
+    this.channel = channel;
+  }
+
+  start() {
+    this.updateClock();
+    setInterval(this.updateClock.bind(this), 500);
+
+    if (this.inBreak()){
+      this.bot.channels.get(this.channel).send('@here Pomodoro! 5 minutitos de RELAX!')
+    }
+  }
+
+  updateClock() {
+    this.messageOnStateChange();
+    this.lastState = this.inBreak() ? "break" : "focus";
+  }
+
+  timeCallback(response) {
+    this.timeDifference = new Date() - new Date(response.datetime);
+  }
+
+  getDate() {
+    return new Date((new Date()).valueOf() + this.timeDifference);
+  }
+
+  getMinutes() {
+    return new Date(new Date() + this.timeDifference).getSeconds();
+  }
+
+  inBreak() {
+    const minutes = this.getDate().getMinutes();
+    return /*(minutes >= 25 && minutes <= 29) || */(minutes >= 55 && minutes <= 59);
+  }
+
+  messageOnStateChange() {
+    if (this.inBreak() && this.lastState == "focus"){
+      this.bot.channels.get(this.channel).send('@here Pomodoro! 5 minutitos de RELAX!')
+    } 
+    else if (!this.inBreak() && this.lastState == "break"){
+      this.bot.channels.get(this.channel).send('@here Se acabó el Pomodoro! ¡A TRABAJAR!')
+    }  
+  }
+}
+
+module.exports = (bot, channel) => new Teamodoro(bot, channel);
